9c1dd52d15d7027f6270f842a4f6c5ba32ec8e34
Nuzet <nabliadzhyiev@gmail.com>
2022-05-02 19:09:56 +0300

    secondcommit

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task1
+{
+    class program
+    {
+        static void Main()
+        {
+            Word w1 = new Word("Слава");
+            Word w2 = new Word("Українi");
+            Word w3 = new Word("Героям ");
+            Word w4 = new Word("Слава");
+            Word w5 = new Word("!");
+
+            Sentence s1 = new Sentence();
+            s1.CreateSentence(w1);
+            s1.CreateSentence(w2);
+            s1.PrintS();
+            Console.WriteLine();
+            
+            Sentence s2 = new Sentence();
+            s2.CreateSentence(w3);
+            s2.CreateSentence(w4);
+            s2.CreateSentence(w5);
+            s2.PrintS();
+            Console.WriteLine();
+
+            Text text = new Text();
+            text.CreateText(s1.sentences);
+            text.CreateText(s2.sentences);
+            text.ShowText();
+            Console.WriteLine();
+            text.AddText("Слава нацiї, Смерть ворогам!");
+            text.ShowText();
+            Console.WriteLine();
+            text.HeaderOfText("Смерть москальнi!");
+            Console.WriteLine();
+
+            
+            
+
+        }
+    }
+}
---
+++ Task2/Credit.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task2
+{
+    class Credit
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t Вибiр кредiту");
+            Console.ResetColor();
+            Console.Write("Введiть час взяття кредiту(в роках): ");
+            double time;
+            bool num = double.TryParse(Console.ReadLine(), out time);
+            if (num == false)
+            {
+                Console.WriteLine("Помилка! Перевiрьте данi!");
+            }
+            else
+            {
+                if (time < 0)
+                {
+                    Console.WriteLine("Помилка! Перевiрьте данi!");
+                }
+                else
+                {
+                    Console.Write("Введiть розмiр кредiту(в $): ");
+                    double size;
+                    bool num2 = double.TryParse(Console.ReadLine(), out size);
+                    if (num2 == false)
+                    {
+                        Console.WriteLine("Помилка! Перевiрьте данi!");
+                    }
+                    else
+                    if (size < 0)
+                    {
+                        Console.WriteLine("Помилка! Перевiрьте данi!");
+                    }
+                    else
+                    {
+                        {
+                            Console.Write("Введiть чи можно погасити кредiт до сроку (True/False): ");
+                            bool e;
+                            bool num3 = bool.TryParse(Console.ReadLine(), out e);
+                            if (num3 == false)
+                            {
+                                Console.WriteLine("Помилка! Перевiрьте данi!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Результат пошуку кредiта за Вашим бажанням: ");
+                                MonoBank mono = new MonoBank();
+                                mono.SearchCredit(time, size, e);
+                                PrivatBank privat = new PrivatBank();
+                                privat.SearchCredit(time, size, e);
+                                AgricoleBank agricole = new AgricoleBank();
+                                agricole.SearchCredit(time, size, e);
+                            }
+                        }
+                    }
+                }
+            
+            }
+        }
+    }
+
+}
